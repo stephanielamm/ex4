2c4e0df4c0713ba509988202053d620bc5130c1f
stephanielamm <slamm5@live.unc.edu>
2017-01-26 18:17:58 -0500

    First commit.
    Added initial files.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// hint -- remove a particular element from an array: http://stackoverflow.com/questions/5767325/how-to-remove-a-particular-element-from-an-array-in-javascript
+// hint -- add an item to an array: http://stackoverflow.com/questions/351409/how-to-append-something-to-an-array
+// hint -- you may need an "editingItem" variable in your data object
+
+new Vue({
+  el: '#app',
+  data: {
+  },
+  methods: {
+  }
+});
