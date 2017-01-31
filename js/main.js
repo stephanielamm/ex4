ebad127e54026a91fc75cc6d6d56e0f92f8d9fd5
stephanielamm <slamm5@live.unc.edu>
2017-01-31 17:51:49 -0500

    Creating a mess.
    Horribly lost in lecture.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ new Vue({
   data: {
     titleInputValue: '',
     contentInputValue: '',
-    editingCards: '',
+    editingItem: '',
 
     cards: [
       {
@@ -20,33 +20,66 @@ new Vue({
       }
     ]
   },
+
+  computed: {
+    editingLabel: function () {
+if (this.editingItem) {
+  return 'Edit Item';
+} else {
+  return 'Create Item';
+}
+},
+      buttonTitle: function () {
+        if (this.editingItem) {
+          return 'Edit Item';
+        } else {
+          return 'Create Item';
+        }
+        },
+
   methods: {
     addTask: function (){
-    this.cards.push({
+      if (this.editingItem) {
+
+
+
+  } else {  this.cards.push({
       title: this.titleInputValue,
       content: this.contentInputValue
     })
+  }
     this.titleInputValue='';
     this.contentInputValue='';
   },
-  //The addTask function took the title input and content values (which are set to data inputs) and pushed them to the Create button
-  //The addTask function then had to reset the title and content input values to blank
-
+  //The addTask function took the title input and content values (which are set to data inputs) and pushed them to the Create button.
+  //The addTask function then had to reset the title and content input values to blank.
   cancel: function() {
+    this.editingItem='';
     this.titleInputValue='';
     this.contentInputValue='';
   },
-  //The cancel function reset the title and input values to blank
-
+  //The cancel function reset the title and input values to blank.
     removeCard: function (thisCard) {
     var cardIndex=this.cards.indexOf(thisCard);
     this.cards.splice(cardIndex,1);
    },
-  //
-   editCard: function (thisCard) {
-     this.editingCards=this.cards.push({
-
+  //Hint -- remove a particular element from an array: http://stackoverflow.com/questions/5767325/how-to-remove-a-particular-element-from-an-array-in-javascript.
+  //To remove a card, need to find the index of the elment you want to remove.
+  //Then use Splice, where you use "start" and "deleteCount."
+  //We want to remove a single item, so use delteCount of 1.
+  //Compute the index by using var cardIndex=this.card.indexOf(thisCard).
+  //Then splice using the new cardIndex and a deleteCount of 1 to delete a single item.
+  //Link this function to the Done button.
+   editCard: function (a) {
+     this.editingItem = a
+     this.currentTitle = a.title
+     this.currentContent = a.content
      });
+//Create a temporary placeholder
+//which can be referenced later
+//to update the item and populate
+//the input filends with the items current values
+
    },
   }
 });
