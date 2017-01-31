c679e6e54744a4f3b974d832bc07cd478532bfe0
stephanielamm <slamm5@live.unc.edu>
2017-01-31 17:23:38 -0500

    Adding annotations. Not for grading.
    Adding annotations for my learning and review purposes.

--- js/main.js
+++ js/main.js
@@ -29,10 +29,15 @@ new Vue({
     this.titleInputValue='';
     this.contentInputValue='';
   },
+  //The addTask function took the title input and content values (which are set to data inputs) and pushed them to the Create button
+  //The addTask function then had to reset the title and content input values to blank
+
   cancel: function() {
     this.titleInputValue='';
     this.contentInputValue='';
   },
+  //The cancel function reset the title and input values to blank
+
     removeCard: function (thisCard) {
     var cardIndex=this.cards.indexOf(thisCard);
     this.cards.splice(cardIndex,1);
